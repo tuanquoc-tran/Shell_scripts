8c9812e0f94e800e5ea8afbef099f4f639509cb5
Tuan Tran <51257497+quoctuan-iot@users.noreply.github.com>
2022-04-03 15:15:26 +0700

    Add files via upload

---
+++ Echo_Option_Color_.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo   "\033[31mNG\033[0m"
+echo   "\033[32mOK\033[0m"
+
+# Black='\033[0;30m'        # Black
+# Red='\033[0;31m'          # Red
+# Green='\033[0;32m'        # Green
+# Yellow='\033[0;33m'       # Yellow
+# Blue='\033[0;34m'         # Blue
+# Purple='\033[0;35m'       # Purple
+# Cyan='\033[0;36m'         # Cyan
+# White='\033[0;37m'        # White
+
+# Echo -option
+ # -n	 do not print the trailing newline.
+ # -e	 enable interpretation of backslash escapes.
+ # \b	 backspace
+ # \\	 backslash
+ # \n	 new line
+ # \r	 carriage return
+ # \t	 horizontal tab
+ # \v	 vertical tab
